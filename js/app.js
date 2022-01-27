472608db9f0621edc6dc67212d33f2cc7c4db6b5
MartaBastida <martabastidasaez@gmail.com>
2022-01-27 09:41:38 +0100

    changes background attachment

--- js/app.js
+++ js/app.js
@@ -23,3 +23,136 @@ $(document).ready(function () {
     compruebaAceptaCookies();
 });
 
+/*window.onload = function(){
+	document.querySelector('#banner-menu').style.backgroundImage = "url('img/homeVerMenu.jpg')";
+	document.querySelector('#banner-menu').stylebackgroundSize = "cover";
+	document.querySelector('#banner-menu').style.backgroundRepeat = "norepeat";
+	document.querySelector('#banner-menu').style.backgroundAttachment = "fixed";
+	document.querySelector('#banner-menu').style.backgroundPosition = "center center";
+}*/
+
+
+
+
+
+
+
+
+
+
+
+//obtiene elementos
+/*const dropdownTitle = document.querySelector('.dropdown .title');
+const dropdownOptions = document.querySelectorAll('.dropdown .option');
+
+//vincula listeners a estos elementos
+dropdownTitle.addEventListener('click', toggleMenuDisplay);
+dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
+document.querySelector('.dropdown .title').addEventListener('change',handleTitleChange);
+
+
+
+function toggleClass(elem,className){
+	if (elem.className.indexOf(className) !== -1){
+		elem.className = elem.className.replace(className,'');
+	}
+	else{
+		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
+	}
+	
+	return elem;
+}
+
+function toggleDisplay(elem){
+	const curDisplayStyle = elem.style.display;			
+				
+	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
+		elem.style.display = 'block';
+	}
+	else{
+		elem.style.display = 'none';
+	}
+}
+
+
+function toggleMenuDisplay(e){
+	const dropdown = e.currentTarget.parentNode;
+	const menu = dropdown.querySelector('.menu');
+	const icon = dropdown.querySelector('.fa-angle-right');
+
+	toggleClass(menu,'hide');
+	toggleClass(icon,'rotate-90');
+}
+
+function handleOptionSelected(e){
+	toggleClass(e.target.parentNode, 'hide');			
+
+	const id = e.target.id;
+	const newValue = e.target.textContent + ' ';
+	const titleElem = document.querySelector('.dropdown .title');
+	const icon = document.querySelector('.dropdown .title .fa');
+
+
+	titleElem.textContent = newValue;
+	titleElem.appendChild(icon);
+	
+	//Activa evento personalizado
+	document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
+	//setTimeout se usa para que la transición se muestre correctamente
+	setTimeout(() => toggleClass(icon,'rotate-90',0));
+}
+
+function handleTitleChange(e){
+	const result = document.getElementById('result');
+
+	result.innerHTML = 'The result is: ' + e.target.textContent;
+}*/
+
+
+
+
+/*const btnBackground = document.querySelector('#btn-change-background');
+const popupBackground = document.querySelector('.popup-wrapper-background');
+const closeBackground = document.querySelector('.popup-close-background');
+
+btnBackground.addEventListener('click', () => {
+    popupBackground.style.display = 'block';
+});
+ 
+closeBackground.addEventListener('click', () => {
+    popupBackground.style.display = 'none';
+});
+ 
+popupBackground.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.className === 'popup-wrapper-background') {
+        popupBackground.style.display = 'none';
+    }
+});
+
+
+
+
+const btnText = document.querySelector('#btn-change-text');
+const popupText = document.querySelector('.popup-wrapper-text');
+const closeText = document.querySelector('.popup-close-text');*/
+ 
+/*window.onload = function(){
+    popupText.style.display = 'none';
+    popupBackground.style.display = 'none';
+}*/
+
+/*btnText.addEventListener('click', () => {
+    popupText.style.display = 'block';
+});
+ 
+closeText.addEventListener('click', () => {
+    popupText.style.display = 'none';
+});
+ 
+popupText.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.className === 'popup-wrapper-text') {
+        popupText.style.display = 'none';
+    }
+});*/
